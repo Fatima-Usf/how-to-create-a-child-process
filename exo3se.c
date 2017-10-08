1ac0a8f4635cc273649c211e0c4bb4e4979ec910
Fatima Usf <f.ucf.eva@gmail.com>
2017-10-09 01:26:43 +0200

    Update exo3se.c

--- exo3se.c
+++ exo3se.c
@@ -4,32 +4,32 @@
 #include<unistd.h>
 
 void executerQuelqueChose (void){
-#define NB_ITERS 5
-int i;
-for (i = 0; i<NB_ITERS; i++){
-printf("i = %d\n", i);
-sleep(rand()%4);
-}}
-
+	#define NB_ITERS 5
+	int i;
+	for (i = 0; i<NB_ITERS; i++){
+		printf("i = %d\n", i);
+		sleep(rand()%4);
+	}
+}
 int main (){
     pid_t p;
-	p=fork();
-if (p>0){
-printf("pid de pére p= %d\n", getpid());
-sleep(30);
+    p=fork();
+    if (p>0){
+	printf("pid de pére p= %d\n", getpid());
+	sleep(30);
+	exit(0);
+    }
 
-exit(0);}
-
-
-else if (p==0){
-printf("pid fils p= %d\n", getpid());
-executerQuelqueChose ();
-printf("p= %d\n", p);
-exit(0);
-}
-else {
-printf("echec");
-exit(0);}
+    else if (p==0){
+	printf("pid fils p= %d\n", getpid());
+	executerQuelqueChose ();
+	printf("p= %d\n", p);
+	exit(0);
+    }
+    else {
+	printf("echec");
+	exit(0);
+    }
 
 return 0;
 }
